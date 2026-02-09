084eb57d7b37e629f075c61b39765a479fc09180
Ev357 <evest.dev@gmail.com>
2026-02-09 23:01:00 +0100

    refactor: improved default config

--- tano-config/src/config.rs
+++ tano-config/src/config.rs
@@ -4,16 +4,29 @@ use serde::Deserialize;
 
 use crate::providers::{ProviderConfig, local::LocalConfig};
 
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Deserialize)]
 pub struct Config {
-    #[serde(default = "default_providers")]
     pub providers: Vec<ProviderConfig>,
 }
 
-fn default_providers() -> Vec<ProviderConfig> {
-    vec![ProviderConfig::Local {
-        config: LocalConfig {
-            path: PathBuf::from("~/Music"),
-        },
-    }]
+impl Default for Config {
+    fn default() -> Self {
+        let expanded_cow = shellexpand::full("~/Music");
+
+        let providers = match expanded_cow {
+            Ok(path) => vec![ProviderConfig::Local {
+                config: LocalConfig {
+                    path: PathBuf::from(path.as_ref()),
+                },
+            }],
+            Err(err) => {
+                tracing::warn!(
+                    "Couldn't find default local provider directory, using empty providers: {err}"
+                );
+                vec![]
+            }
+        };
+
+        Self { providers }
+    }
 }
