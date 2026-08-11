d4cbefb83570c2a32c10ae7cbc09dda714ca2994
Ev357 <evest.dev@gmail.com>
2026-08-11 22:41:24 +0200

    perf: added nix fenix cache

--- flake.nix
+++ flake.nix
@@ -4,10 +4,12 @@
   nixConfig = {
     extra-substituters = [
       "https://tano.cachix.org"
+      "https://fenix.cachix.org"
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
       "tano.cachix.org-1:Vi3qOkAzZY0PVB7ca3Kp03gURYrEFK7gNdX2thodn7I="
+      "fenix.cachix.org-1:ecJhr+RdYEdcVgUkjruiYhjbBloIEGov7bos90cZi0Q="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
