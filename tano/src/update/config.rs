2749b078e27f6b87d58e56413c32327a69c604f5
Ev357 <evest.dev@gmail.com>
2026-08-11 22:33:22 +0200

    refactor: removed debug traces

--- tano/src/update/config.rs
+++ tano/src/update/config.rs
@@ -1,5 +1,4 @@
 use tano_config::actor::msg::ConfigMsg;
-use tano_macro::trace_dbg;
 use tokio::sync::watch;
 
 use crate::{
@@ -15,9 +14,7 @@ pub fn update_config(model_tx: &watch::Sender<Model>, config_msg: ConfigMsg) -> 
     match config_msg {
         ConfigMsg::ConfigLoaded(config) => match config {
             Ok(config) => {
-                trace_dbg!(&config);
                 let watch_state = ConfigWatchState::resolve();
-                trace_dbg!(&watch_state);
                 model_tx.send_modify(|model| model.config = ConfigState::Loaded(watch_state));
 
                 Cmd::Msg(Msg::InitProviders {
